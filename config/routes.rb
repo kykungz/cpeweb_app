6ce0dad7d15f9f2457f76aaf8a96676b635f4e80
Kongpon Charanwattanakit <kongpon.c@ku.th>
2017-05-25 07:59:10 +0000

    edit page style

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
     root 'static_pages#home'
     get '/about', to: 'static_pages#about'
     get '/contact', to: 'static_pages#contact'
+    get '/cpe_course', to: 'static_pages#cpe_course'
 end
